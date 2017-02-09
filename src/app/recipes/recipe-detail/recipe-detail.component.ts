c7da4936972d6e760d85e5478d96b6fbf923ac4c
Adrian Wyss <adrianroman.wyss@bfh.ch>
2017-02-09 09:52:25 +0100

    fixed thumbnails

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -7,7 +7,7 @@ import {Recipe} from "../recipe";
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() selectedRecipe: Recipe;
+  @Input() selected: Recipe;
 
 
   constructor() { }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -27,7 +27,6 @@ export class RecipeListComponent implements OnInit {
 
     this.recipeSelected.emit(recipe);
 
-
   }
 
 }
